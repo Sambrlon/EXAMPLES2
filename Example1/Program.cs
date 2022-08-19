556ab4f2603f372f7a70bfd97249d50dede7270c
Artem <yarilin98@mail.ru>
2022-08-19 23:25:31 +0300

    добавил второй способ нахождения квадрата числа

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -5,3 +5,7 @@ int number = int.Parse(Console.ReadLine());
 //Решение математическим путем
 int sqr = number * number;
 Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
+//Решение с использованием библиотеки
+int sqr2 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа2 {number} равен {sqr2}");
