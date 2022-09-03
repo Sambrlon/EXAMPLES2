b6040b77318191bbce36503e61fc59d99560b8a4
Artem <yarilin98@mail.ru>
2022-09-03 14:50:21 +0300

    Added 21

---
+++ Sem3_Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве
+
+Console.Clear();
+
+Console.WriteLine("Input A(x;y)");
+Console.Write("X1:");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Y1:");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input B(x;y)");
+Console.Write("X2:");
+int x2 = Convert.ToInt16(Console.ReadLine());
+Console.Write("Y2:");
+int y2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
