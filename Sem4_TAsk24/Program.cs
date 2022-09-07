2b889a74b50445b8341a96d75fbd6cdcec84871e
Artem <yarilin98@mail.ru>
2022-09-08 00:04:55 +0300

    Seminar 4 Task: 25, 27,29

---
+++ Sem4_TAsk24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа которая принимает на вход число (А) 
+//и выдает сумму чисел от 1 до (А)
+// 7->28
+//4->10
+
+Console.Clear();
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSum(A));
+
+int GetSum(int a)
+{
+    int sum = 0;
+    for(int i = 0; i <= a; i++)
+    {
+        sum +=i;
+    }
+    return sum;
+}
---
+++ Sem4_Task24(2)/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 26: Прога которая принимает на вход число и выдаёт количество цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine());
+
+string a = Convert.ToString(A);
+Console.WriteLine(a.Length);
